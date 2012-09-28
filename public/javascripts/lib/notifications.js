da9511ad92de91b5c536d9047f9352500ef08897
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-09-28 14:32:06 -0400

    refactor

--- public/javascripts/lib/notifications.js
+++ public/javascripts/lib/notifications.js
@@ -1,18 +1,42 @@
 define(['jquery'], function($) {
   'use strict';
+  $(loader);
 
-  var toggle_button = $('#notifications');
+  var notifier = window.webkitNotifications;
 
-  $(document).ready( function() {
-    if (window.webkitNotifications.checkPermission() !== 0) { // 0 is PERMISSION_ALLOWED
+  var loader = function() {
+    var toggle_button = $('#notifications');
+    if ( !notification_allowed() ) {
       toggle_button.show();
     }
-  });
 
-  toggle_button.on('click', function() {
-    if (window.webkitNotifications.checkPermission() !== 0) { // 0 is PERMISSION_ALLOWED
-      window.webkitNotifications.requestPermission();
+    toggle_button.on('click', function() {
+      if ( !notification_allowed() ) {
+        notifier.requestPermission();
+      }
+    });
+
+    notify("Test", "Test Message");
+  };
+
+  var notify = function(title, message) {
+    try {
+      if ( notification_allowed() ) {
+        // no icon for now
+        notifier.createNotification(null, 'NoodleApp - ' + title, message);
+      }
+    } catch(error) {
+      console.log(error);
     }
-  });
+  };
+
+  var notification_allowed = function() {
+    var PERMISSION_ALLOWED = 0;
+    if ( notifier.checkPermission() === PERMISSION_ALLOWED ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 });
